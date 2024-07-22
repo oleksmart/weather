f1b4d4e7d6cdf843bac1a0e8cc21d28213a90ac7
Oleks Mart <alx.woland@gmail.com>
2024-07-22 15:55:37 +0300

    asdasd

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 
 app = Flask(__name__)
+variable = "Hello"
+stations = pd.read_csv("data/stations.txt", skiprows=17)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -17,6 +19,22 @@ def about(station, date):
     result_dictionary = {"station": station, "date": date, "temperature": temperature}
     return result_dictionary
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/year/<station>/<year>")
+def yearly(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
